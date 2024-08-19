448c47eac7012d66a8f78ebe9e30502b30c7f310
Mahrad <mahradti@gmail.com>
2024-08-19 16:44:16 +0330

    Configure method added

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
 class ProjectInput {
     templateElement: HTMLTemplateElement;
+
     hotsElement: HTMLDivElement;
 
     element:HTMLFormElement;
+
+    titleInputElement:HTMLInputElement;
+
+    descriptionInputElement:HTMLTextAreaElement;
+
+    peopleInputElement:HTMLInputElement;
 
     constructor() {
         this.templateElement = document.getElementById("project-input")! as HTMLTemplateElement;
@@ -13,9 +20,29 @@ class ProjectInput {
         console.log(importedNode);
 
         this.element = importedNode.firstElementChild as HTMLFormElement
+        this.element.id = "user-input"
+
+        this.titleInputElement = this.element.querySelector("#title") as HTMLInputElement
+
+        this.descriptionInputElement = this.element.querySelector("#description") as HTMLTextAreaElement
+
+        this.peopleInputElement = this.element.querySelector("#people") as HTMLInputElement
+        
+        this.configure()
+
         this.attach();
     }
-    
+
+    private submitHandler(event:Event){
+        event.preventDefault()
+        console.log(this.titleInputElement.value);
+        
+    }
+
+
+    private configure(){
+        this.element.addEventListener("submit" , this.submitHandler.bind(this))
+    }
 
     private attach() {
         this.hotsElement.insertAdjacentElement("afterbegin", this.element)
