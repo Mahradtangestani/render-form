2e2904dfd222eb721aecbf137a0c9392d0068cd0
Mahrad <mahradti@gmail.com>
2024-08-24 17:28:50 +0330

    class State added

--- src/app.ts
+++ src/app.ts
@@ -17,17 +17,31 @@ class Project {
     }
 }
 
-type Listener = (items:Project[]) => void
+type Listener<T> = (items:T[]) => void
 
-class ProjectState {
 
-    private listeners: Listener[] = []
+class State<T>{
+    
+    protected listeners: Listener<T>[] = []
+
+    addListener(listenerFn: Listener<T>) {
+        this.listeners.push(listenerFn)
+    }
+}
+
+
+
+class ProjectState extends State<Project> {
+
+    
 
     private projects: Project[] = []
 
     private static instance: ProjectState
 
-    private constructor() { }
+    private constructor() { 
+        super()
+    }
 
     static getInstace() {
         if (this.instance) {
@@ -37,9 +51,7 @@ class ProjectState {
         return this.instance;
     }
 
-    addListener(listenerFn: Listener) {
-        this.listeners.push(listenerFn)
-    }
+    
 
     addProject(title: string, description: string, numOfPeople: number) {
         const newProject = new Project(
